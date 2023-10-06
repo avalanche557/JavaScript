9deaa061ae7823fc53a0b6f2b58e28f8eb6becb2
avalanche557 <abhinavkarn55@gmail.com>
2023-10-07 00:57:26 +0530

    7th oct 2023

--- curry_function.js
+++ curry_function.js
@@ -13,3 +13,25 @@ sum(1)
 sum(4)
 sum()
 
+// curry function - 2
+
+const sum_curry = (...args) => {
+  let storage = [...args]
+  //base case is if there is no arguments
+  if(storage.length === 0) {
+    return 0
+  } 
+  const temp = function(...args2) {
+    storage.push(...args2)
+    if(args2.length === 0) {
+      return storage.reduce((a, b) => a+b, 0)
+    } else {
+      return temp;
+    }
+  }
+  return temp
+}
+
+
+let total = sum_curry(10)(20)(30)()
+console.log(total)

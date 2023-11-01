7e98473be0345a99d4b5527bf86903bc603261e4
avalanche557 <abhinavkarn55@gmail.com>
2023-11-02 00:48:07 +0530

    2nd Nov, 2023

--- questiohooks/src/App.js
+++ questiohooks/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useRef, useState} from 'react'
+import { useCallback, useEffect, useRef, useState} from 'react'
+
+
+//code for usePrevious hooks
 
 const usePrevious = (val) => {
   const ref = useRef();
@@ -10,17 +13,36 @@ const usePrevious = (val) => {
   return ref.current
 }
 
+//code for useToggle hooks
+
+const useToggle = (arr, index) => {
+  const [currentIndex, setCurrentIndex] = useState(index)
+
+  const toggle = useCallback(() => {
+    return setCurrentIndex((prevIndex) => prevIndex > arr.length - 1 ?  0 : prevIndex + 1)
+  }, [arr])
+
+  return [toggle, currentIndex]
+}
+
 
 function App() {
   const [count, setCount] = useState(0)
   const previousCount = usePrevious(count)
+  const [toogle, value] = useToggle([1,2,3,4,5], 2)
   return (
     <div className="App">
       <p>Count: {count}</p>
       <p>Previous Count: {previousCount}</p>
       <button onClick={() => setCount(count + 1)}>Increase Count</button>
+      <div>
+        <p>Value: {value}</p>
+        <button onClick={toogle}>Toggle</button>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
